a0184b70e36076ccd16000f007ee90e0c8549f68
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-11 07:44:28 -0300

    feat: adding prisma

--- server.ts
+++ server.ts
@@ -1,19 +1,8 @@
 import express, { Request, Response } from "express";
-import { Pool, QueryResult } from 'pg';
+import { PrismaClient } from '@prisma/client';
 import cors from "cors";
 import helmet from "helmet";
 import bcrypt from "bcrypt";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-});
 
 const app = express();
 
@@ -21,55 +10,61 @@ app.use(helmet());
 app.use(express.json());
 app.use(cors());
 
+const prisma = new PrismaClient({
+  datasources: {
+    default: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
 const saltRounds: number = 10;
 
 app.post("/register", async (req: Request, res: Response) => {
   try {
-    const email = req.body.email;
-    const password = req.body.password;
-    const userName = req.body.userName;
+    const { email, password, userName } = req.body;
 
-    const result: QueryResult = await pool.query(
-      "SELECT * FROM users WHERE email=$1",
-      [email]
-    );
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
 
-    if (result.rows.length === 0) {
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-      await pool.query(
-        "INSERT INTO users (userName, email, password) VALUES ($1, $2, $3)",
-        [userName, email, hashedPassword]
-      );
-
-      res.send({ msg: "Cadastrado com sucesso" });
-    } else {
-      res.status(400).send({ msg: "Usuário já cadastrado" });
+    if (existingUser) {
+      return res.status(400).send({ msg: "Usuário já cadastrado" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = await prisma.user.create({
+      data: {
+        userName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.send({ msg: "Cadastrado com sucesso" });
   } catch (error) {
     console.error(error);
     res.status(500).send("Erro no servidor");
   }
 });
 
+
 app.post("/login", async (req: Request, res: Response) => {
   try {
-    const email = req.body.email;
-    const password = req.body.password;
+    const { email, password } = req.body;
 
-    const result: QueryResult = await pool.query(
-      "SELECT * FROM users WHERE email = $1",
-      [email]
-    );
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-    if (result.rows.length > 0) {
-      const isMatch = await bcrypt.compare(password, result.rows[0].password);
+    if (!user) {
+      return res.status(400).send({ msg: "Email ou senha incorretos" });
+    }
 
-      if (isMatch) {
-        res.send({ msg: "Usuário logado com sucesso" });
-      } else {
-        res.status(400).send({ msg: "Email ou senha incorretos" });
-      }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      res.send({ msg: "Usuário logado com sucesso" });
     } else {
       res.status(400).send({ msg: "Email ou senha incorretos" });
     }
