065d8115a9576dee36fe514a4b0dfd3b4aa2990a
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-12 17:45:35 -0300

    feat: addinf cart in db

--- server.ts
+++ server.ts
@@ -1,12 +1,10 @@
-import { PrismaClient } from '@prisma/client';
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-// import helmet from 'helmet';
-import bcrypt from 'bcrypt';
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bcrypt from "bcrypt";
 
 const app = express();
 
-// app.use(helmet());
 app.use(express.json());
 app.use(cors());
 
@@ -38,6 +36,13 @@ app.post("/register", async (req: Request, res: Response) => {
         userName,
         email,
         password: hashedPassword,
+        cart: {
+          create: {
+            cartItems: {
+              create: [],
+            },
+          },
+        },
       },
     });
 
@@ -70,6 +75,58 @@ app.post("/login", async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("Erro no servidor");
+  }
+});
+app.post("/add-to-cart", async (req: Request, res: Response) => {
+  const { userId, cartItems } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { cart: { include: { cartItems: true } } },
+    });
+
+    if (!user) {
+      return res.status(404).send({ msg: "User not found" });
+    }
+
+    const cartId = user.cart?.id;
+
+    if (!cartId) {
+      return res.status(404).send({ msg: "Cart not found for the user" });
+    }
+
+    const cartItemsArray: {
+      imgSrc: string;
+      title: string;
+      price: string;
+      productId: string;
+      quantity: number;
+    }[] = cartItems.map((item: any) => {
+      return {
+        imgSrc: item.imgSrc,
+        title: item.title,
+        price: item.price,
+        productId: item.productId,
+        quantity: item.quantity,
+      };
+    });
+
+    await prisma.cartItem.createMany({
+      data: cartItemsArray.map((item) => ({
+        cartId: cartId,
+        imgSrc: item.imgSrc,
+        title: item.title,
+        price: item.price,
+        productId: item.productId,
+        quantity: item.quantity,
+      })),
+    });
+
+    res.send({ msg: "Items added to the cart successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
