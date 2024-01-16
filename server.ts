ef488f7feb5245b39f54a0be7c4a380e4340f47d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-16 19:34:05 -0300

    feat: updating server

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
 const jwt = require("jsonwebtoken");
@@ -12,6 +12,27 @@ app.use(cors());
 const prisma = new PrismaClient();
 
 const saltRounds: number = 10;
+
+interface AuthenticatedRequest extends Request {
+  user?: { id: number };
+}
+
+app.use((req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_PASS || "");
+      req.user = decoded;
+    } catch (error) {
+      console.error(error);
+      res.status(401).send({ msg: "Unauthorized" });
+      return;
+    }
+  }
+
+  next();
+});
 
 app.post("/register", async (req: Request, res: Response) => {
   try {
@@ -68,6 +89,7 @@ app.post("/login", async (req: Request, res: Response) => {
 
     if (isMatch) {
       res.send({
+        userName: user.userName,
         userId: user.id,
         token: token,
         msg: "Usuário logado com sucesso",
@@ -81,9 +103,67 @@ app.post("/login", async (req: Request, res: Response) => {
   }
 });
 
-app.post("/add-to-cart", async (req: Request, res: Response) => {
+app.get("/get-user", async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const userId = req.query.userId;
+
+    if (!userId) {
+      return res.status(400).send({ msg: "User ID not provided" });
+    }
+
+    const userIdNumber = parseInt(userId as string, 10);
+
+    if (isNaN(userIdNumber)) {
+      return res.status(400).send({ msg: "Invalid User ID" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userIdNumber },
+    });
+    
+
+    if (!user) {
+      return res.status(404).send({ msg: "User not found" });
+    }
+
+    res.send({ cart: user });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/get-cart", async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const userId = req.query.userId;
+
+    if (!userId) {
+      return res.status(400).send({ msg: "User ID not provided" });
+    }
+
+    const userIdNumber = parseInt(userId as string, 10);
+
+    if (isNaN(userIdNumber)) {
+      return res.status(400).send({ msg: "Invalid User ID" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userIdNumber },
+      include: { cart: { include: { cartItems: true } } },
+    });
+
+    if (!user) {
+      return res.status(404).send({ msg: "User not found" });
+    }
+
+    res.send({ cart: user });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+app.post("/add-to-cart", async (req: AuthenticatedRequest, res: Response) => {
   const { userId, cartItems } = req.body;
-  console.log("Incoming request body:", req.body);
 
   try {
     if (!userId) {
@@ -115,31 +195,70 @@ app.post("/add-to-cart", async (req: Request, res: Response) => {
     }
 
     const cartItemsArray = cartItems.map((item: any) => ({
+      cartId: cartId!,
       imgSrc: item.imgSrc,
       title: item.title,
       price: item.price,
       productId: item.productId,
       quantity: item.quantity,
     }));
-    
+
     await prisma.cartItem.createMany({
-      data: cartItemsArray.map((item: any) => ({
-        cartId: cartId!,
-        imgSrc: item.imgSrc,
-        title: item.title,
-        price: item.price,
-        productId: item.productId,
-        quantity: item.quantity,
-      })),
+      data: cartItemsArray,
     });
 
-    res.send({ msg: "Itens adicionado no carrinho" });
+    res.send({ msg: "Itens adicionados no carrinho", cartId });
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
   }
 });
+app.delete(
+  "/remove-from-cart",
+  async (req: AuthenticatedRequest, res: Response) => {
+    try {
+      const userId = req.query.userId;
 
+      const userIdNumber = parseInt(userId as string, 10);
+
+      if (isNaN(userIdNumber)) {
+        return res.status(400).send({ msg: "Invalid User ID" });
+      }
+
+      const productId = req.query.productId;
+
+      const user = await prisma.user.findUnique({
+        where: { id: userIdNumber },
+        include: { cart: { include: { cartItems: true } } },
+      });
+
+      if (!user) {
+        return res.status(404).send({ msg: "User not found" });
+      }
+
+      if (productId) {
+        const cartItem = user.cart!.cartItems.find(item => item.productId === productId);
+
+        if (!cartItem) {
+          return res.status(404).send({ msg: "Product not found in the cart" });
+        }
+
+        await prisma.cartItem.delete({
+          where: {
+            id: cartItem.id,
+          },
+        });
+
+        res.send({ msg: "Item removed from cart", cartId: user.cart!.id });
+      } else {
+        return res.status(400).send({ msg: "Product ID is required" });
+      }
+    } catch (error) {
+      console.error("Error processing request:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Rodando na porta ${PORT}`);
