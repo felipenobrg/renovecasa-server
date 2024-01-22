fd0440780d3d5a09409fe6079b37db4cd6e9253d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-22 17:01:26 -0300

    feat: improving cor.use

--- server.ts
+++ server.ts
@@ -7,7 +7,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "renovecasajp.com",
+  credentials: true,
+}));
 
 const prisma = new PrismaClient();
 
